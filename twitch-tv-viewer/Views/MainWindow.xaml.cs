9be60527099761b780e4df27aea9ddf0c5666d98
dukemiller <dukemiller928@hotmail.com>
2016-10-22 19:57:14 -0700

    Add invocations for views on mainwindow

--- twitch-tv-viewer/Views/MainWindow.xaml.cs
+++ twitch-tv-viewer/Views/MainWindow.xaml.cs
@@ -1,19 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
+﻿using System.Windows;
 
-namespace twitch_tv_viewer
+namespace twitch_tv_viewer.Views
 {
     /// <summary>
     /// Interaction logic for MainWindow.xaml
@@ -24,5 +11,9 @@ namespace twitch_tv_viewer
         {
             InitializeComponent();
         }
+
+        private void Edit_Click(object sender, RoutedEventArgs e) => new Edit().ShowDialog();
+
+        private void Add_Click(object sender, RoutedEventArgs e) => new Add().ShowDialog();
     }
 }
