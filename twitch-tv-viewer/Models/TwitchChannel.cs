453ab008a53862d265aa43fd93d721a59e2ad268
dukemiller <dukemiller928@hotmail.com>
2016-10-23 20:23:55 -0700

    Move model to model folder

--- twitch-tv-viewer/Models/TwitchChannel.cs
+++ twitch-tv-viewer/Models/TwitchChannel.cs
@@ -1,12 +1,9 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Diagnostics;
-using System.Linq;
-using System.Text;
 using System.Threading.Tasks;
 using Newtonsoft.Json.Linq;
 
-namespace twitch_tv_viewer.Classes
+namespace twitch_tv_viewer.Models
 {
     public class TwitchChannel : IComparable
     {
