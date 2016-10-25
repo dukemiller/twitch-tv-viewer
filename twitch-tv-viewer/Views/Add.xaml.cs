913c04c0eb6c12d7bddf378883c2ab5c6a2b26ca
dukemiller <dukemiller928@hotmail.com>
2016-10-25 00:54:11 -0700

    Implement add's viewmodel into add view

--- twitch-tv-viewer/Views/Add.xaml.cs
+++ twitch-tv-viewer/Views/Add.xaml.cs
@@ -11,6 +11,7 @@ using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Shapes;
+using twitch_tv_viewer.ViewModels;
 
 namespace twitch_tv_viewer.Views
 {
@@ -21,6 +22,7 @@ namespace twitch_tv_viewer.Views
     {
         public Add()
         {
+            DataContext = new AddViewModel {Close = Close};
             InitializeComponent();
         }
     }
