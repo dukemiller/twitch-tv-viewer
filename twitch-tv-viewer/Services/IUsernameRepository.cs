c9ff5fd696322d453ac1f8a9217e8bb1e5d986ad
dukemiller <dukemiller928@hotmail.com>
2016-10-25 00:12:09 -0700

    Add interface and implementation of username repository

---
+++ twitch-tv-viewer/Services/IUsernameRepository.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.Generic;
+
+namespace twitch_tv_viewer.Services
+{
+    internal interface IUsernameRepository
+    {
+        List<string> GetUsernames();
+        void AddUsername(string username);
+        void RemoveUsername(string username);
+    }
+}
---
+++ twitch-tv-viewer/Services/UsernameRepository.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections.Generic;
+using System.Collections.Specialized;
+using System.Linq;
+using twitch_tv_viewer.Properties;
+
+namespace twitch_tv_viewer.Services
+{
+    internal class UsernameRepository : IUsernameRepository
+    {
+        public UsernameRepository()
+        {
+            if (Settings.Default.Usernames == null)
+                Settings.Default.Usernames = new StringCollection();
+        }
+
+        public List<string> GetUsernames()
+        {
+            return Settings.Default.Usernames.Cast<string>().ToList();
+        }
+
+        public void AddUsername(string username)
+        {
+            Settings.Default.Usernames.Add(username.ToLower());
+            Settings.Default.Save();
+        }
+
+        public void RemoveUsername(string username)
+        {
+            Settings.Default.Usernames.Remove(username.ToLower());
+            Settings.Default.Save();
+        }
+    }
+}
