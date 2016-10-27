6ddb6aa8054eeaf99fe29499bd5d83ec3c2f9d0a
dukemiller <dukemiller928@hotmail.com>
2016-10-26 20:45:08 -0700

    Add placeholder components view/viewmodel files

---
+++ twitch-tv-viewer/ViewModels/Components/NotificationAreaViewModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using GalaSoft.MvvmLight;
+
+namespace twitch_tv_viewer.ViewModels.Components
+{
+    class NotificationAreaViewModel : ViewModelBase
+    {
+    }
+}
