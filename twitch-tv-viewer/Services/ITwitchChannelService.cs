6a825051b462e0343d806bea5e0ed065bfea92f3
dukemiller <dukemiller928@hotmail.com>
2016-10-26 20:50:55 -0700

    Create twitch channel video viewer service

---
+++ twitch-tv-viewer/Services/ITwitchChannelService.cs
@@ -0,0 +1,11 @@
+﻿using System.Threading.Tasks;
+using twitch_tv_viewer.Models;
+
+namespace twitch_tv_viewer.Services
+{
+    interface ITwitchChannelService
+    {
+        Task<string> PlayVideo(TwitchChannel twitchChannel);
+        void OpenChat(TwitchChannel twitchChannel);
+    }
+}
---
+++ twitch-tv-viewer/Services/TwitchChannelService.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using twitch_tv_viewer.Models;
+
+namespace twitch_tv_viewer.Services
+{
+    public class TwitchChannelService : ITwitchChannelService
+    {
+        public async Task<string> PlayVideo(TwitchChannel channel)
+        {
+            var startInfo = new ProcessStartInfo
+            {
+                FileName = "livestreamer",
+                Arguments = $"--http-query-param client_id=spyiu9jqdnfjtwv6l1xjk5zgt8qb91l twitch.tv/{channel.Name} high",
+                UseShellExecute = false,
+                RedirectStandardOutput = true,
+                CreateNoWindow = true
+            };
+
+            var process = new Process
+            {
+                StartInfo = startInfo
+            };
+
+            process.Start();
+
+            return await process.StandardOutput.ReadToEndAsync();
+        }
+
+        public void OpenChat(TwitchChannel channel)
+        {
+            Process.Start($"http://twitch.tv/{channel.Name}/chat?popout=");
+        }
+    }
+}
