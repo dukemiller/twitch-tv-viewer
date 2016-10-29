79b382f8882186c88a4c4a7c6faaf93476b4688a
dukemiller <dukemiller928@hotmail.com>
2016-10-29 15:24:26 -0700

    Add previewkeydown/textinput view logic for input validation

--- twitch-tv-viewer/Views/Add.xaml.cs
+++ twitch-tv-viewer/Views/Add.xaml.cs
@@ -1,4 +1,6 @@
-﻿using System.Windows;
+﻿using System.Text.RegularExpressions;
+using System.Windows;
+using System.Windows.Input;
 using twitch_tv_viewer.ViewModels;
 
 namespace twitch_tv_viewer.Views
@@ -12,6 +14,22 @@ namespace twitch_tv_viewer.Views
         {
             DataContext = new AddViewModel {Close = Close};
             InitializeComponent();
+        }
+
+        private static readonly Regex Regex = new Regex("^[a-zA-Z_0-9]+$");
+
+        private void OnPreviewTextInput(object sender, TextCompositionEventArgs e)
+        {
+            if (!Regex.IsMatch(e.Text))
+                e.Handled = true;
+            base.OnPreviewTextInput(e);
+        }
+
+        private void OnPreviewKeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.Key == Key.Space)
+                e.Handled = true;
+            base.OnPreviewKeyDown(e);
         }
     }
 }
--- twitch-tv-viewer/Views/Edit.xaml.cs
+++ twitch-tv-viewer/Views/Edit.xaml.cs
@@ -1,5 +1,7 @@
-﻿using System.Text.RegularExpressions;
+﻿using System.Linq;
+using System.Text.RegularExpressions;
 using System.Windows;
+using System.Windows.Controls;
 using System.Windows.Input;
 using twitch_tv_viewer.ViewModels;
 
@@ -10,9 +12,11 @@ namespace twitch_tv_viewer.Views
     /// </summary>
     public partial class Edit : Window
     {
+        private readonly EditViewModel _editViewModel;
         public Edit()
         {
-            DataContext = new EditViewModel {Close = Close};
+            _editViewModel = new EditViewModel { Close = Close };
+            DataContext = _editViewModel;
             InitializeComponent();
         }
 
@@ -23,6 +27,17 @@ namespace twitch_tv_viewer.Views
             if (!Regex.IsMatch(e.Text))
                 e.Handled = true;
             base.OnPreviewTextInput(e);
+        }
+        
+        private void OnPreviewKeyDown(object sender, KeyEventArgs e)
+        {
+            var str = _editViewModel.Usernames;
+            var charBefore = str.Substring(str.Length - 2, 1);
+            var lastCharacter = str.Substring(str.Length-1, 1);
+            var isComma = lastCharacter.Equals(",");
+            if (e.Key == Key.Space && !isComma || e.Key == Key.OemComma && (isComma || charBefore.Equals(",")))
+                e.Handled = true;
+            base.OnPreviewKeyDown(e);
         }
     }
 }
