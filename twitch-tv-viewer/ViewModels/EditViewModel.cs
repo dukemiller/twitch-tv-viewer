927d84ae6262c23d670bb85a528c8ba6a2d891cc
dukemiller <dukemiller928@hotmail.com>
2016-10-29 02:32:03 -0700

    Implement edit view and viewmodel

---
+++ twitch-tv-viewer/ViewModels/EditViewModel.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Linq;
+using System.Text.RegularExpressions;
+using GalaSoft.MvvmLight;
+using GalaSoft.MvvmLight.CommandWpf;
+using twitch_tv_viewer.Repositories;
+
+namespace twitch_tv_viewer.ViewModels
+{
+    internal class EditViewModel : ViewModelBase
+    {
+        private readonly UsernameRepository _user;
+        private string _usernames;
+
+        public EditViewModel()
+        {
+            _user = new UsernameRepository();
+            Usernames = string.Join(", ", _user.GetUsernames());
+            ConfirmCommand = new RelayCommand(Confirm);
+            CancelCommand = new RelayCommand(Cancel);
+        }
+
+        public RelayCommand ConfirmCommand { get; set; }
+
+        public RelayCommand CancelCommand { get; set; }
+
+        public Action Close { private get; set; }
+
+        private void Confirm()
+        {
+            var usernames = Regex
+                .Split(Regex.Replace(Usernames, @"\s+", @" "), ", ")
+                .Where(s => s.Length > 0);
+            _user.SetUsernames(usernames);
+            MessengerInstance.Send(new ResetMessage());
+            Close();
+        }
+
+        private void Cancel() => Close();
+
+        public string Usernames
+        {
+            get { return _usernames; }
+            set
+            {
+                _usernames = value; 
+                RaisePropertyChanged();
+            }
+        }
+    }
+}
--- twitch-tv-viewer/Views/Edit.xaml.cs
+++ twitch-tv-viewer/Views/Edit.xaml.cs
@@ -1,4 +1,7 @@
-﻿using System.Windows;
+﻿using System.Text.RegularExpressions;
+using System.Windows;
+using System.Windows.Input;
+using twitch_tv_viewer.ViewModels;
 
 namespace twitch_tv_viewer.Views
 {
@@ -9,7 +12,17 @@ namespace twitch_tv_viewer.Views
     {
         public Edit()
         {
+            DataContext = new EditViewModel {Close = Close};
             InitializeComponent();
+        }
+
+        private static readonly Regex Regex = new Regex("^[a-zA-Z_0-9,]+$");
+
+        private void OnPreviewTextInput(object sender, TextCompositionEventArgs e)
+        {
+            if (!Regex.IsMatch(e.Text))
+                e.Handled = true;
+            base.OnPreviewTextInput(e);
         }
     }
 }
