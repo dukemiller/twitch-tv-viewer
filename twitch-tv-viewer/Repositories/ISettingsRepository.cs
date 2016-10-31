6ddc36c164b431cd2a52e4f62192919be8ee604a
dukemiller <dukemiller928@hotmail.com>
2016-10-31 10:47:10 -0700

    Create settings repo class (essentially wrapper for app settings)

---
+++ twitch-tv-viewer/Repositories/ISettingsRepository.cs
@@ -0,0 +1,7 @@
+﻿namespace twitch_tv_viewer.Repositories
+{
+    public interface ISettingsRepository
+    {
+        string Quality { get; set; }
+    }
+}
---
+++ twitch-tv-viewer/Repositories/SettingsRepository.cs
@@ -0,0 +1,11 @@
+﻿namespace twitch_tv_viewer.Repositories
+{
+    internal class SettingsRepository: ISettingsRepository
+    {
+        public string Quality
+        {
+            get { return Properties.Settings.Default.Quality; }
+            set { Properties.Settings.Default.Quality = value; }
+        }
+    }
+}
