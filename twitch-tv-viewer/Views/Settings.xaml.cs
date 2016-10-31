e4a7ea3af4cacd5a4eba1078591b98f12c187173
dukemiller <dukemiller928@hotmail.com>
2016-10-31 10:57:13 -0700

    Create initial settings view

---
+++ twitch-tv-viewer/Views/Settings.xaml.cs
@@ -0,0 +1,16 @@
+﻿using System.Windows;
+using twitch_tv_viewer.ViewModels;
+
+namespace twitch_tv_viewer.Views
+{
+    /// <summary>
+    /// Interaction logic for Settings.xaml
+    /// </summary>
+    public partial class Settings : Window
+    {
+        public Settings()
+        {
+            InitializeComponent();
+        }
+    }
+}
